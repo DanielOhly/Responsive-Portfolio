a2e142eb75590f5019a72328398253ef7dffe1be
DanielOhly <38668083+DanielOhly@users.noreply.github.com>
2019-02-17 20:20:52 -0600

    Portfolio update

--- portfolio.js
+++ portfolio.js
@@ -19,7 +19,7 @@
             words: 
             "<p>Development drives me, creative problem solving while working in the confines of a particular language is a gratifying thing.</p>"+
             "<p>I love learning new languages and development techniques, and working towards mastery in them. I&rsquo;m a huge fan of poring over documentation, and gaining greater understanding."+
-            "<p>Lately I have been focusing on Vanilla JS, and database schema.</p>"+
+            "<p>Lately I have been focusing on Angular, lots of Vanilla JS, and database schema.</p>"+
             "<p>I spent most of my adult life in the beer industry. Starting in college, schlepping kegs and emptying ashtrays in a German beer hall. In the next 12 years I did every job in the industry imaginable and picked up a lot of skills along the way. Staffing and HR, Office Management, Distribution Arrangement, Cost Analysis, Vendor Management, Event Planning, Contract Negotiation, you name it."+ 
             "No matter what the job entailed specifically, I always stuck to what I saw as my true north:</p>"+
             "<ul><b>Work smart, not hard:</b> Think things through, find the best solutions before you jump head first.</ul>"+
@@ -32,16 +32,17 @@
         }
         const projects = {
             home: "Projects",
-            words: "<div class=\"content\"><div id= \"projecttitlename\"><b>Home Movies</b></div>" +
+            words: 
+                "<div class=\"content\"><div id= \"projecttitlename\"><b> In Progress: Inesland </b>" + "</div>" +
+                "<p> An attempt for my D&D group to keep their character data straight if they leave their character sheets at home. Also as a useful tool for first time D&D players to learn the ins and outs of my favorite game!<p>" +
+                "<p> It is currently utilizing Firebase and I am moving the whole thing over to Angular, and building an API to store user input. <p>"+
+                "<p> Check out my progress on my GitHub here: <a style=\"text-decoration: underline\"target= \"blank\"href=\"https://github.com/DanielOhly/inesland\">Inesland</a></p>"+
+                "<div class= \"image-content\"><a target= \"blank\"href=\"secret.html\"><img id=\"image\" src=\"assets/images/ithicwoods.png\"></a></div>"+
+                "<div class=\"content\"><div id= \"projecttitlename\"><b>Home Movies</b></div>" +
                 "<p>An interactive satire site meant to engage people to watch movies together, at other people&rsquo;s homes. An attempt to get people to socialize, in a farscial manner.</p>" +
                 "<p>The big lessons learned here were reading a whole lot of documentation, manipulating DOM, and database utilization. </p>"+
                 "<p>Utilizing JavaScript, JSON, Firebase, JQuery, Bootstrap, Google Maps API and OMDB API</p><a target=\"blank\" href=\"https://github.com/DanielOhly/homeMovies\">See the code here.</a> The image will launch the site.</div>" +
                 "<div class= \"image-content\"><a target= \"blank\"href=\"https://danielohly.github.io/homeMovies/\"><img id=\"image\" src=\"assets/images/homemovies.png\"></a></div>" +
-                "<div class=\"content\"><div id= \"projecttitlename\"><b> In Progress: The Heroes of Inesland </b>" + "</div>" +
-                "<p> An attempt for my D&D group to keep their character data straight if they leave their character sheets at home. Also as a method of memorializing former campaigns.<p>" +
-                "<p> It is currently utilizing Firebase and JavaScript, focusing on a RESTful API to store user input. I am working towards it being a useful tool for first time D&D players to learn the ins and outs of my favorite game!<p>"+
-                "<p> Check out my progress on my GitHub here: <a style=\"text-decoration: underline\"target= \"blank\"href=\"secret.html\">Inesland</a></p>"+
-                "<div class= \"image-content\"><a target= \"blank\"href=\"secret.html\"><img id=\"image\" src=\"assets/images/ithicwoods.png\"></a></div>"+
                 "<div class=\"content\"><div id= \"projecttitlename\"><b> Nenas Arepa Kitchen </b>" + "</div>" +
                 "<p> A simple single page website for a local independent food company<p>" +
                 "<p> The goal was to put together a straightforward website that would give the client what they needed, without being too busy or info dense.</p>"+
